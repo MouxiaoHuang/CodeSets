cd11e1b8e5c788fa2acbd1d360be89b5f3e45900
MouxiaoHuang <neohuang469@gmail.com>
2022-07-13 02:38:26 +0800

    update

--- t.py
+++ t.py
@@ -1,32 +1,28 @@
+from turtle import forward
 import numpy as np
 
-class MaxPool2d():
-    def __init__(self, kernel_size = (2, 2), stride = 1) -> None:
-        self.stride = stride
-        self.kernel_size_h = kernel_size[0]
-        self.kernel_size_w = kernel_size[1]
+class BatchNorm2d():
+    def __init__(self, eps = 0.001, momentum = 0.99, beta = 0, gamma = 1) -> None:
+        self.eps = eps
+        self.momentum = momentum
+        self.beta = beta
+        self.gamma = gamma
+        self.training_mean = 0.0
+        self.training_var = 1.0
 
-    def forward(self, x):
-        self.channel, self.height, self.width = x.shape[0], x.shape[1], x.shape[2]
-        self.out_h = (self.height - self.kernel_size_h) // self.stride + 1
-        self.out_w = (self.width - self.kernel_size_w) // self.stride + 1
-        self.output = np.zeros((self.channel, self.out_h, self.out_w), dtype=np.uint8)
+    def forward_train(self, input):
+        # input is [bz, c, h, w]
+        self.mean = input.mean(axis = 0)
+        self.var = input.var(axis = 0)
+        input_norm = (input - self.mean) / (self.var + self.eps)
+        res = self.gamma * input_norm + self.beta
 
-        for c_index in range(self.channel):
-            out_h_index = 0
-            for h_index in range(0, self.height, self.stride):
-                out_w_index = 0
-                for w_index in range(0, self.width, self.stride):
-                    try:
-                        self.output[c_index, out_h_index, out_w_index] = np.max(x[c_index, h_index : h_index + self.kernel_size_h, w_index : w_index + self.kernel_size_w])
-                    except:
-                        break
-                    out_w_index += 1
-                out_h_index += 1
-        return self.output
+        self.training_mean = self.momentum * self.mean + (1 - self.momentum) * self.mean
+        self.training_var = self.momentum * self.var + (1 - self.var) * self.var
 
-maxpool = MaxPool2d(kernel_size=(2, 1), stride=1)
-x = np.arange(1, 28).reshape((3, 3, 3))
-output = maxpool.forward(x)
-print(x)
-print(output)
+        return res
+
+    def forward_inference(self, input):
+        input_norm = (input - self.training_mean) / (self.training_var + self.eps)
+        res = self.gamma * input_norm + self.beta
+        return res
