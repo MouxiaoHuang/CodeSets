a8eb8caf29cc628614b35fa2f6be242acda63ce4
MouxiaoHuang <neohuang469@gmail.com>
2022-08-30 19:15:50 +0800

    update

--- t.py
+++ t.py
@@ -1,84 +1,34 @@
-
-in_time = []
-out_time = []
-pre = ''
-
-def calculate(s_in, s_out):
-    a = int(s_out[0:2]) - int(s_in[0:2])
-    b = int(s_out[3:5]) - int(s_in[3:5])
-    c = int(s_out[6:8]) - int(s_in[6:8])
-    if c < 0:
-        c = 60 + c
-        b = b - 1
-    if b < 0:
-        b = 60 + c
-        a = a - 1
-    res = ''
-    if a < 10:
-        res = '0' + str(a)
-    else:
-        res = str(a)
-    if b < 10:
-        res = res + ':0' + str(b)
-    else:
-        res = res + ':' + str(b)
-    if c < 10:
-        res = res + ':0' + str(c)
-    else:
-        res = res + ':' + str(c)
-    return res
-
-def cal_sum(s1, s2):
-    a = int(s1[0:2]) + int(s2[0:2])
-    b = int(s1[3:5]) + int(s2[3:5])
-    c = int(s1[6:8]) + int(s2[6:8])
-    if c >= 60:
-        c = c - 60
-        b += 1
-    if b >= 60:
-        b = b - 60
-        a += 1
-    res = ''
-    if a < 10:
-        res = '0' + str(a)
-    else:
-        res = str(a)
-    if b < 10:
-        res = res + ':0' + str(b)
-    else:
-        res = res + ':' + str(b)
-    if c < 10:
-        res = res + ':0' + str(c)
-    else:
-        res = res + ':' + str(c)
-    return res
+# n = int(input())
+n = 4
+# times = [[1, 2], [2, 3]]
+# times = [[1, 3], [2, 4]]
+times = [[1, 2], [2, 3], [3, 4], [3, 8]]
 
 
-n = int(input())
-for _ in range(n):
-    s = input().split()
-    if s[0] == 'out':
-        if not in_time or pre == 'out':
-            continue
+def process_input():
+    n = int(input())
+    tmp = input().split(' ')
+    times = []
+    for i in range(0, len(tmp), 2):
+        time = [int(tmp[i]), int(tmp[i + 1])]
+        times.append(time)
+    return n, times
+
+
+def solution(n, times):
+    times.sort(key=lambda x: x[0])
+    res = []
+    c = 1
+    for tmp in times:
+        if not res or tmp[0] >= res[-1][1]:
+            res.append(tmp)
         else:
-            out_time.append(s[1])
-            pre = 'out'
-    elif s[0] == 'in':
-        if pre == 'in':
-            continue
-        else:
-            in_time.append(s[1])
-            pre = 'in'
-# print(in_time)
-# print(out_time)
-sum_time = []
-for i in range(len(in_time)):
-    sum_time.append(calculate(in_time[i], out_time[i]))
+            res[-1][1] = max(res[-1][1], tmp[1])
+            c += 1
+    # print(len(res))
+    print(res, c)
+    return len(res)
 
-while True:
-    if len(sum_time) == 1:
-        break
-    else:
-        sum_time[1] = cal_sum(sum_time[0], sum_time[1])
-        sum_time.pop(0)
-print(sum_time[0])
+
+# n, times = process_input()
+print(solution(n, times))
